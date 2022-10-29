629ba61af26abc073e4bd38d19ed63a8072f507f
Gleison Batista <gleisonbs@gmail.com>
2022-10-29 14:53:22 -0300

    fix: import chi v5

--- handlers/get.go
+++ handlers/get.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/gleisonbs/todo-api/models"
+	"github.com/go-chi/chi/v5"
 )
 
 func Get(w http.ResponseWriter, r *http.Request) {
--- handlers/update.go
+++ handlers/update.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 
 	"github.com/gleisonbs/todo-api/models"
+	"github.com/go-chi/chi/v5"
 )
 
 func Update(w http.ResponseWriter, r *http.Request) {
